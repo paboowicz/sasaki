4941d6b0db2f177b42595e8549cb0ac9e3631187
Łukasz Lechowicz <llechowicz@gmail.com>
2023-04-07 18:42:27 +0200

    js

--- js/main.js
+++ js/main.js
@@ -581,3 +581,18 @@ new WOW().init();
 	}
 
 })(jQuery);
+
+function showArticle(idx) {
+    const services = document.getElementsByClassName("service-detail");
+	const links = document.getElementsByClassName("menu-link");
+
+    for (let i=0; i<services.length; i++) {
+        services[i].setAttribute("class", "service-detail hidden");
+        links[i].setAttribute("class", "menu-link");
+
+    }
+    document.getElementById("service-detail-" + idx).setAttribute("class", "service-detail");
+    document.getElementById("menu-link-" + idx).setAttribute("class", "menu-link active");
+
+
+}
